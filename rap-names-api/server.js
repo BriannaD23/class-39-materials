31e2c7488394b97752a29aa5f489af3a6a6a52a3
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2023-11-10 11:54:14 -0800

    changed name

--- rap-names-api/server.js
+++ rap-names-api/server.js
@@ -2,10 +2,43 @@ const express = require('express')
 const app = express()
 const PORT = 8000;
 
+
+const rappers = {
+    '21 Savage':{
+        'age': 29,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+
+    'Nicki Minaj':{
+        'age': 40,
+        'birthName': 'Onika Tayna Maraj',
+        'birthLocation': 'Tabago, Trinidad'
+    },
+
+    "Drake":{
+        'age': 37,
+        'birthName': 'Aubrey Drake Graham',
+        'birthLocation': 'Toronto, Canada'
+    }
+
+
+}
+
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
+app.get('/api/:name', (request, response) => {
+    const rapperName = request.params.name
+    if(rappers[rapperName]){
+        response.json(rappers[rapperName])
+    }else{
+        response.json(rappers['unknown'])
+    }
+})
+
 app.listen( PORT, () => {
+    
     console.log(`The server is now running on port ${PORT}!`)
 })
